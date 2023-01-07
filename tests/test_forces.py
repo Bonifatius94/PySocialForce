d78e8ecacd7a01b745e2ef47973dc420149b47fa
Marco Tröster <marco@troester-gmbh.de>
2023-01-07 22:35:23 +0100

    refactor tests

--- tests/test_forces.py
+++ tests/test_forces.py
@@ -1,9 +1,11 @@
+from typing import Tuple
+
 import numpy as np
 import pytest
 
+from pysocialforce.utils.config import SimulatorConfig
 from pysocialforce import forces
 from pysocialforce import Simulator
-from pysocialforce.utils import DefaultConfig
 
 
 @pytest.fixture()
@@ -16,12 +18,11 @@ def generate_scene():
     return scene, scene.config
 
 
-def test_desired_force(generate_scene):
+def test_desired_force(generate_scene: Tuple[Simulator, SimulatorConfig]):
     scene, config = generate_scene
-    f = forces.DesiredForce()
-    f.init(scene, config)
-    f.factor = 1.0
-    assert f.get_force(debug=True) == pytest.approx(
+    f = forces.DebuggableForce(forces.DesiredForce(config.desired_force_config, scene.peds))
+    config.desired_force_config.factor = 1.0
+    assert f(debug=True) == pytest.approx(
         np.array(
             [
                 [-3.83847763, -1.83847763],
@@ -34,12 +35,11 @@ def test_desired_force(generate_scene):
     )
 
 
-def test_social_force(generate_scene):
+def test_social_force(generate_scene: Tuple[Simulator, SimulatorConfig]):
     scene, config = generate_scene
-    f = forces.SocialForce()
-    f.init(scene, config)
-    f.factor = 1.0
-    assert f.get_force(debug=True) == pytest.approx(
+    f = forces.DebuggableForce(forces.SocialForce(config.social_force_config, scene.peds))
+    config.social_force_config.factor = 1.0
+    assert f(debug=True) == pytest.approx(
         np.array(
             [
                 [3.18320152e-12, 1.74095049e-12],
@@ -52,36 +52,33 @@ def test_social_force(generate_scene):
     )
 
 
-def test_group_rep_force(generate_scene):
+def test_group_rep_force(generate_scene: Tuple[Simulator, SimulatorConfig]):
     scene, config = generate_scene
     scene.peds.groups = [[1, 0], [3, 2]]
-    f = forces.GroupRepulsiveForce()
-    f.init(scene, config)
-    f.factor = 1.0
-    assert f.get_force(debug=True) == pytest.approx(
+    f = forces.DebuggableForce(forces.GroupRepulsiveForce(config.group_repulsive_force_config, scene.peds))
+    config.group_repulsive_force_config.factor = 1.0
+    assert f(debug=True) == pytest.approx(
         np.array([[0.0, -0.1], [0.0, 0.1], [0.0, -0.01], [0.0, 0.01], [0.0, 0.0]])
     )
 
 
-def test_group_coherence_force(generate_scene):
+def test_group_coherence_force(generate_scene: Tuple[Simulator, SimulatorConfig]):
     scene, config = generate_scene
     scene.peds.groups = [[0, 1, 3], [2, 4]]
-    f = forces.GroupCoherenceForce()
-    f.init(scene, config)
-    f.factor = 1.0
-    assert f.get_force(debug=True) == pytest.approx(
+    f = forces.DebuggableForce(forces.GroupCoherenceForce(config.group_coherence_force_config, scene.peds))
+    config.group_coherence_force_config.factor = 1.0
+    assert f(debug=True) == pytest.approx(
         np.array([[0.0, 0.0], [0.0, 0.0], [0.0, 1.0], [-0.71421284, -0.69992858], [0.0, -1.0],])
     )
 
 
-def test_group_gaze_force(generate_scene):
+def test_group_gaze_force(generate_scene: Tuple[Simulator, SimulatorConfig]):
     scene, config = generate_scene
     scene.peds.groups = [[0, 1, 3], [2, 4]]
-    f = forces.GroupGazeForce()
-    f.init(scene, config)
-    f.config.config["fov_phi"] = 100
-    f.factor = 1.0
-    assert f.get_force(debug=True) == pytest.approx(
+    f = forces.DebuggableForce(forces.GroupGazeForce(config.group_gaze_force_config, scene.peds))
+    config.group_gaze_force_config.fov_phi = 100
+    config.group_gaze_force_config.factor = 1.0
+    assert f(debug=True) == pytest.approx(
         np.array(
             [
                 [0.96838684, 0.96838684],
